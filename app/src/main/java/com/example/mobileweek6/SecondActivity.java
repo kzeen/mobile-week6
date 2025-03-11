d3559e2bfd3998cbcc1b37211416c0ebe2dd5953
zeen <157370421+kzeen@users.noreply.github.com>
2025-03-11 22:56:03 +0200

    Add countries listview to second activity layout

--- app/src/main/java/com/example/mobileweek6/SecondActivity.java
+++ app/src/main/java/com/example/mobileweek6/SecondActivity.java
@@ -2,6 +2,8 @@ package com.example.mobileweek6;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SecondActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<String> countries;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,13 @@ public class SecondActivity extends AppCompatActivity {
 
         setSupportActionBar(findViewById(R.id.my_toolbar)); // Used to set custom action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Adds a back button, parent (where we go back to) defined in manifest
+
+        listView = findViewById(R.id.lv_countries);
+        listView.setNestedScrollingEnabled(true);
+        String[] ar = getResources().getStringArray(R.array.countries);
+        countries = new ArrayList<>(Arrays.asList(ar));
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        listView.setAdapter(adapter);
     }
 
     @Override
