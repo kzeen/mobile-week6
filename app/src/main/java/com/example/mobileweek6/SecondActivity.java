5ec56c18765e3b8272a06644a2bea3d110d95d70
zeen <157370421+kzeen@users.noreply.github.com>
2025-03-11 23:01:49 +0200

    Add search functionality

--- app/src/main/java/com/example/mobileweek6/SecondActivity.java
+++ app/src/main/java/com/example/mobileweek6/SecondActivity.java
@@ -2,17 +2,22 @@ package com.example.mobileweek6;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -46,6 +51,31 @@ public class SecondActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.search_menu, menu);
 
+        MenuItem searchitem = menu.findItem(R.id.mi_search);
+        SearchView searchview = (SearchView) searchitem.getActionView();
+        searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                filter_countries(query);
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter_countries(newText);
+                return false;
+            }
+        });
+
         return true;
+    }
+
+    private void filter_countries(String prefix) {
+        Toast.makeText(this.getApplicationContext(), prefix, Toast.LENGTH_SHORT).show();
+        List<String> filtered_countries;
+        filtered_countries = countries.stream().filter(s ->s.toLowerCase().startsWith(prefix.toLowerCase())).collect(Collectors.toList());
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, filtered_countries);
+        listView.setAdapter(adapter);
     }
 }
